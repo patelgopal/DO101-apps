0432fd1782d4521cb75a895834e43a6aab52f396
patelgopal <patelgopal@yahoo.com>
2021-07-12 12:47:35 +0530

    Update app.js

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -2,7 +2,7 @@ var express = require('express');
 app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World from Openshift!\n');
+  res.send('Hello World from DO101-Openshift!\n');
 });
 
 app.listen(8080, function () {
